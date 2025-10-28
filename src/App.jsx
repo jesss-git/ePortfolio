954dda5cb293b2ac5409c9bbc2b7fa3e04a2d88c
jesss-git <jessicachan@Jessicas-MacBook-Pro.local>
2025-10-28 02:49:20 -0400

    Setup all new Repos

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { CodePage } from "eportfolio-code";
 import { UXUIPage } from "eportfolio-uxui";
 import { GraphicsPage } from "eportfolio-graphics";
+import { GalleryPage } from "eportfolio-gallery";
 
 import Home from "./pages/Home/Home";
 import About from "./pages/About";
 import Connect from './pages/Connect';
-
-// import UXUI from "./pages/UXUI";
-// import Graphics from "./pages/Graphics";
-import Gallery from "./pages/Gallery";
-// import Code from "./pages/Code/Code";
-
 
 
 function App() {
@@ -31,7 +26,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path="/ux-ui" element={<UXUIPage />} />
           <Route path="/graphics" element={<GraphicsPage />} />
-          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/gallery" element={<GalleryPage />} />
           <Route path="/code" element={<CodePage />} />
           <Route path="/about" element={<About />} />
           <Route path="/connect" element={<Connect />} />
