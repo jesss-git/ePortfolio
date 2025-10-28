2882562918a38ee2871f50501fd6cebdea2f68db
jesss-git <jessicachan@Jessicas-MacBook-Pro.local>
2025-10-28 02:18:59 -0400

    Setup UXUI Repo

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,17 @@ import { Navbar, Footer } from "eportfolio-shared";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import { CodePage } from "eportfolio-code";
+import { UXUIPage } from "eportfolio-uxui";
 
 import Home from "./pages/Home/Home";
-import UXUI from "./pages/UXUI";
+import About from "./pages/About";
+import Connect from './pages/Connect';
+
+// import UXUI from "./pages/UXUI";
 import Graphics from "./pages/Graphics";
 import Gallery from "./pages/Gallery";
 // import Code from "./pages/Code/Code";
-import About from "./pages/About";
-import Connect from './pages/Connect'
+
 
 
 function App() {
@@ -25,7 +28,7 @@ function App() {
 
         <Routes>
           <Route path='/' element={<Home />} />
-          <Route path="/ux-ui" element={<UXUI />} />
+          <Route path="/ux-ui" element={<UXUIPage />} />
           <Route path="/graphics" element={<Graphics />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/code" element={<CodePage />} />
